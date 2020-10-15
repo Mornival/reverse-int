18e07de42bd4b89e7c8f93f2205ae60d63ecf41f
Mornival <55550567+Mornival@users.noreply.github.com>
2020-10-15 21:04:13 +0300

    init: create function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let n1 = n.toString();
+    let str = "";
+    for(let i = n1.length - 1; i >= 0; i--){
+        if(n1[i] !== '-')
+        {
+            str += n1[i];
+        }
+    }
+    return str;
 }
